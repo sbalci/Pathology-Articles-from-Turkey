84d97b7d3683328b29eb25ad07595558f775dca3
Serdar Balcı <drserdarbalci@gmail.com>
2020-12-03 22:02:48 +0300

    copied PBPath files

--- searchPubMed.R
+++ searchPubMed.R
@@ -5,52 +5,53 @@ print(paste0("Daily PBPath Journal Watch from PubMed Started at: ", Sys.time()))
 
 # load library ----
 print(paste0("Load Library Started at: ", Sys.time()))
-# source("R/loadLibrary.R")
+source("R/loadLibrary.R")
 print(paste0("Load Library Ended at: ", Sys.time()))
 # Get PMID list from PubMed ----
 ## search topic ----
 print(paste0("define topics Started at: ", Sys.time()))
-# source("R/topic.R")
+source("R/topic.R")
 print(paste0("define topics Ended at: ", Sys.time()))
 ## journal list ----
 print(paste0("Read Journal List Started at: ", Sys.time()))
-# source("R/journals.R")
+source("R/journals.R")
 print(paste0("Read Journal List Ended at: ", Sys.time()))
 ## searchQuery ----
 print(paste0("Read searchQuery Started at: ", Sys.time()))
-# source("R/searchQuery.R")
+source("R/searchQuery.R")
 print(paste0("Read searchQuery Ended at: ", Sys.time()))
 ## date range ----
-# source("R/dateRange.R")
+source("R/dateRange.R")
 ## generate search formula ----
 print(paste0("Read searchFormula Started at: ", Sys.time()))
-# source("R/searchFormula.R")
+source("R/searchFormula.R")
 print(paste0("Read searchFormula Ended at: ", Sys.time()))
 
 # Search ----
 print(paste0("Search Started at: ", Sys.time()))
-# source("R/search.R")
+source("R/search.R")
 print(paste0("Search Ended at: ", Sys.time()))
 
 # Fetch ----
 print(paste0("Fetch Started at: ", Sys.time()))
-# source("R/fetch.R")
+source("R/fetch.R")
 print(paste0("Fetch Ended at: ", Sys.time()))
 
 # Get PMIDs ----
 print(paste0("Get PMIDs Started at: ", Sys.time()))
-# source("R/getPMID.R")
+source("R/getPMID.R")
 print(paste0("Get PMIDs Ended at: ", Sys.time()))
 
 
 # Save PMIDs ----
 print(paste0("Save PMIDs Started at: ", Sys.time()))
-# source("R/savePMID.R")
+source("R/savePMID.R")
 print(paste0("Save PMIDs Ended at: ", Sys.time()))
 
 # Save dataframes ----
 print(paste0("Save DFs Started at: ", Sys.time()))
-# source("R/saveDF.R")
+source("R/saveDF.R")
 print(paste0("Save DFs Ended at: ", Sys.time()))
 
 sink()
+
